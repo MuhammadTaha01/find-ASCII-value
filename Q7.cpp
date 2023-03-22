04df713c7213915b2dcc79c1dc5a241a7daf15f6
Muhammad Taha <127325759+MuhammadTaha01@users.noreply.github.com>
2023-03-22 23:30:48 +0500

    Add files via upload

---
+++ Q7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Enter an ASCII value: ";
+    cin >> x;
+
+    cout<<endl;
+
+    while(x<=5)
+    {
+        cout <<x<<endl;
+        x++;
+    }
+
+    return 0;
+
+}
